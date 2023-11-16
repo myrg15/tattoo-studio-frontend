0398c17243ef2c7e32a4ae0539f9b6838a3eb77e
myrg15 <merishield@gmail.com>
2023-11-16 05:48:29 +0100

    feat: add customInput

---
+++ src/common/CustomInput/CustomInput.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Box, Button, Typography, TextField } from "@mui/material"
+import InputController from "../../common/Inputs/InputController"
+
+export const CustomInput = ({disabled, design, type, name, placeholder, value, functionProp, functionBlur}) => {
+     return (
+        <div>
+        <TextField
+            disabled={disabled}
+            className={design}
+            type={type}
+            name={name}
+            placeholder={placeholder}
+            value={value || undefined}
+            onChange={(e)=>functionProp(e)}
+            onBlur={(e)=>functionBlur(e)}
+         />
+        </div>
+
+     )
+}
