854def8627be309eaa58c32e12be7cd0ebc6bfd4
myrg15 <merishield@gmail.com>
2023-11-18 12:32:04 +0100

    feat:insert function create appointment

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,31 +1,124 @@
-import { Box, Button, InputLabel, MenuItem, Select, Typography, Grid} from "@mui/material";
+import {
+  Box,
+  Button,
+  InputLabel,
+  MenuItem,
+  Select,
+  Typography,
+  Grid,
+  Modal,
+  FormControl,
+} from "@mui/material";
 import React, { useEffect, useState } from "react";
 //import * as React from "react";
 import axiosInstance from "../../utils/axios";
 import { useNavigate } from "react-router-dom";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
-import { getAllDesing } from "../../services/apiCalls";
-
+import { createAppointment, getAllArtist, getAllDesing } from "../../services/apiCalls";
+import InputController from "../../common/Inputs/InputController";
 
 //falta LOGICA
+
+const HORAS_DISPONIBLES = ["09:00", "12:00", "15:00", "18:00"];
+
 export const Home = () => {
   const [designs, setDesigns] = useState([]);
+  const [artist, setArtist] = useState([]);
+  const [open, setOpen] = React.useState(false);
+  const [selectHour, setSelectHour] = useState("")
+  const [selectArtist, setSelectArtist] = useState("")
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   useEffect(() => {
     const data = async () => {
       const response = await getAllDesing();
+      const artist = await getAllArtist();
+      setArtist(artist);
       setDesigns(response);
     };
 
     data();
   }, []);
 
+  const seleccionHour = (e) => {
+    setSelectHour(e.target.value)
+  };
+  const seleccionArtist = (e) => {
+    setSelectArtist(e.target.value)
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    const data = {
+      time : selectHour,
+      tattooartist_id : selectArtist,
+      service : 'tattoo'
+    }
+
+    await createAppointment(data)
+   
+  };
+
   return (
     <Box display="flex" gap="10px" padding="20px" sx={{ cursor: "pointer" }}>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box
+          width="300px"
+          bgcolor="white"
+          position="absolute"
+          top="50%"
+          left="50%"
+          sx={{ transform: "translate(-50%, -50%)" }}
+          padding="15px"
+        >
+          <Box
+            component="form"
+            display="flex"
+            flexDirection="column"
+            gap="10px"
+            onSubmit={onSubmit}
+          >
+            <FormControl fullWidth>
+              <InputLabel sx={{ bgcolor: "white" }}>
+                Horas Disponibles
+              </InputLabel>
+              <Select fullWidth value={selectHour} onChange={seleccionHour}>
+                {HORAS_DISPONIBLES.map((item) => {
+                  return <MenuItem value={item}>{item}</MenuItem>;
+                })}
+              </Select>
+            </FormControl>
+            <FormControl fullWidth>
+              <InputLabel sx={{ bgcolor: "white" }}>Artistas</InputLabel>
+              <Select fullWidth value={selectArtist} onChange={seleccionArtist}>
+                {artist.map((item) => {
+                  return (
+                    <MenuItem value={item.user_id}>
+                      {item.user.username}
+                    </MenuItem>
+                  );
+                })}
+              </Select>
+            </FormControl>
+            <Button type="submit" variant="contained">
+              Reservar
+            </Button>
+          </Box>
+        </Box>
+      </Modal>
       <Grid container spacing={3}>
-      {designs.map((item) => (
-        <Grid key={item.id} item xs={12} sm={6} md={4} lg={3}>
-         <Box
+        {designs.map((item) => (
+          <Grid key={item.id} item xs={12} sm={6} md={4} lg={3}>
+            <Box
               sx={{
                 width: "100%",
                 height: "200px",
@@ -44,6 +137,7 @@ export const Home = () => {
               />
               <Button
                 variant="contained"
+                onClick={handleOpen}
                 sx={{
                   position: "absolute",
                   bottom: "10px",
@@ -60,5 +154,5 @@ export const Home = () => {
     </Box>
   );
 };
-       
+
 export default Home;
--- src/services/apiCalls.js
+++ src/services/apiCalls.js
@@ -1,37 +1,64 @@
-//aqui debo poner todas mis rutas 
-//export const register 
+//aqui debo poner todas mis rutas
+//export const register
 
-import axiosInstance from "../utils/axios"
-
+import axiosInstance from "../utils/axios";
 
 //export const logUser = asyn (boby)
 
 export const login = async (data) => {
-    try {
-        const res = await axiosInstance.post('/users/login', data)
-        localStorage.setItem('token', res.data.token)
-        localStorage.setItem('username', res.data.username)
-        localStorage.setItem('role', res.data.role)
-        window.location.href = '/'
-    } catch (error) {
-        console.log(error)
-    }
-} 
+  try {
+    const res = await axiosInstance.post("/users/login", data);
+    localStorage.setItem("token", res.data.token);
+    localStorage.setItem("username", res.data.username);
+    localStorage.setItem("role", res.data.role);
+    window.location.href = "/";
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const registerUser = async (data) => {
-   try {
+  try {
     const res = await axiosInstance.post("/users/register", data);
-   } catch (error) {
-        console.log(error)
-   }
-}
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-export const getAllDesing = async() => {
-    try {
-        const res = await axiosInstance.get("/desingallery/all");
-        return res.data.desings
-    } catch (error) {
-        console.log(error)
-    }
-}
+export const getAllDesing = async () => {
+  try {
+    const res = await axiosInstance.get("/desingallery/all");
+    return res.data.desings;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
+export const getAllArtist = async () => {
+  try {
+    const response = await axiosInstance.get("/artist/all");
+    return response.data.artist;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const createAppointment = async (data) => {
+  const token = localStorage.getItem('token');
+  console.log(token)
+
+  try {
+    const response = await axiosInstance.post(
+      "/appointment/appointment_create",
+      data,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    console.log(response.data);
+  } catch (error) {
+    console.log(error);
+  }
+};
